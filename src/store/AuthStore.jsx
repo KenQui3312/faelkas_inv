6d24f4968c721aac4ac99e8f6aea7b5be77f667b
KenQui3312 <kenetadiel@gmail.com>
2025-10-06 12:33:18 -0600

    commit 6-10-25

---
+++ src/store/AuthStore.jsx
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+import { supabase } from "../index"
+
+export const useAuthStore=create((set,get)=>({
+    signInWithEmail: async(p)=>{
+        const {data,error} = await supabase.auth.signInWithPassword({
+            email: p.correo,
+            password: p.pass
+        })
+        if(error){
+            return null;
+        }
+        if (data) return data;
+    },
+    signOut: async ()=>{
+        const {error} = await supabase.auth.signOut()
+        if(error)
+            throw new Error("A ocurrido un error durante el cierre de sesion"+error)
+    }
+}))
