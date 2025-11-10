474cb1a3e17adbaed52c5d28e512f0f895f55964
KenQui3312 <kenetadiel@gmail.com>
2025-11-09 18:27:31 -0600

    commit 9-11-25 18:27

--- src/components/templates/HomeTemplate.jsx
+++ src/components/templates/HomeTemplate.jsx
@@ -1,18 +1,26 @@
 import styled from "styled-components";
+import {  BannerHome } from "../../index";
+import { useState } from "react";
+
 export function HomeTemplate() {
+
   return (
-    <Container>
-      <h1>Home template</h1>
-    </Container>
+    <Main>
+      <BannerHome/>
+    </Main>
   );
 }
-const Container = styled.div`
+const Main = styled.main`
+  min-height: 100vh;
+  width: 100%;
+  background-color: ${(props) => props.theme.bgtotal};
+  color: ${({ theme }) => theme.text};
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  position: relative;
   overflow: hidden;
-  background-color: ${(props) => props.theme.bgtotal};
-  color: ${({ theme }) => theme.text};
-  width: 100%;
+  font-size:26px;
+  
 `;
+
