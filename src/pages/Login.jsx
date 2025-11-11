48f66b95a1f7da83671dc69c292713b8b233237b
Manuel Antonio Rivera Corrales <mana_riverac42@unicah.edu>
2025-11-10 20:26:01 -0600

    DOC Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,22 @@
 import styled from "styled-components";
-import {LoginTemplate} from "../index"
+// Importa styled-components para definir estilos CSS en JS.
+// En este archivo se define un contenedor `Container` al final, aunque actualmente no se usa.
+
+import { LoginTemplate } from "../index";
+// Importa el componente visual LoginTemplate, que contiene la estructura de la pantalla de login.
+
 export function Login() {
+  // Componente funcional principal que representa la pantalla de login.
+  // Su propósito es renderizar el template de login.
 
   return (
     <>
-    <LoginTemplate />
+      {/* Se renderiza el componente LoginTemplate */}
+      <LoginTemplate />
     </>
   );
 }
+
+// Definición de un contenedor estilizado usando styled-components.
+// Actualmente está vacío, pero se puede usar para envolver contenido o agregar estilos futuros.
 const Container = styled.div``;
