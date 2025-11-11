09cb012a96b2e9959b09441311c124c985b3960f
Manuel Antonio Rivera Corrales <mana_riverac42@unicah.edu>
2025-11-10 20:28:55 -0600

    DOC Reportes.jsx

--- src/pages/Reportes.jsx
+++ src/pages/Reportes.jsx
@@ -1,6 +1,16 @@
-import {  ReportesTemplate} from "../index";
+import { ReportesTemplate } from "../index";
+// Importa el template visual ReportesTemplate, que contiene la estructura y diseño
+// de la pantalla de reportes dentro del sistema.
+
 export function Reportes() {
+  // Componente funcional principal llamado "Reportes".
+  // Su función es actuar como contenedor y renderizar el template correspondiente
+  // a la pantalla de reportes del sistema.
+
   return (
-   <ReportesTemplate/>
+    <>
+      {/* Se renderiza el componente ReportesTemplate */}
+      <ReportesTemplate />
+    </>
   );
 }
