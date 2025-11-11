798883ad2ef0bd12ed0989f14b8071fc267a7727
KenQui3312 <kenetadiel@gmail.com>
2025-11-10 18:02:05 -0600

    commit 10-11-25

--- src/components/atomos/Icono.jsx
+++ src/components/atomos/Icono.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 export const Icono = styled.span`
-    color: ${({theme})=>theme.text};
+   color: ${(props)=>props.theme.text};
     font-size:20px;
+  
+  
 `
